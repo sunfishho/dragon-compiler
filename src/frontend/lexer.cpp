0243470418824f19dd9da7668ccd4f11c2bf8d08
Matthew Ho <homattieho@gmail.com>
2021-11-20 23:52:05 -0500

    Added error messages (bugs still possible).

--- src/frontend/lexer.cpp
+++ src/frontend/lexer.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <variant>
 #include <map>
+#include <utility>
 
 // using namespace std;
 
@@ -16,18 +17,26 @@ enum Op {logor, logand, noteq, eq, leq, geq, lt,
 enum AssignmentOp {plusequals, minusequals, assignequals};
 enum Enclosures {lparen, rparen, lcurly, rcurly, lsquare, rsquare};
 enum Punctuation {comma, semicolon};
-//expand on this later
-enum Errors {illegalchar, missingquotes};
 
-//  Your scanner should note illegal characters, missing quotation marks, and other lexical errors with reasonable error 
-// messages. The scanner should find as many lexical errors as possible, and should be able to continue scanning after 
-// errors are found. The scanner should also filter out comments and whitespace not in string and character literals.
+struct IllegalChar{ 
+    char unexpectedChar; 
+};
 
+struct MissingQuotes{
+};
+
+struct UnexpectedEOF{
+};
+
+using Error = std::variant<IllegalChar, MissingQuotes, UnexpectedEOF, std::monostate>;
 
 struct Token{
     Tag tag;
-    std::variant<int32_t, std::string, std::monostate, Op, AssignmentOp, Enclosures, Punctuation, Errors> value;
+    std::variant<int32_t, std::string, std::monostate, Op, AssignmentOp, Enclosures, Punctuation> value;
 };
+
+//The error field should be std::monostate if there isn't an error
+using TokenWithError = std::pair<Token, Error>;
 
 std::vector<std::pair<std::string, Token>> symbolMatcher = { {"||", {oper, logor}}, 
                                                              {"&&", {oper, logand}},
@@ -89,10 +98,32 @@ std::string printTag(Tag t){
 }
 
 std::ostream &operator<<(std::ostream &os, const Token &token){   
-    std::cout << std::setw(15) << std::left << printTag(token.tag) << std::visit(make_string_functor(), token.value);
+    std::cout << std::setw(15) << std::left << printTag(token.tag) << std::visit(make_string_functor(), token.value) << std::endl;
     return os;
 }
 
+std::string printErrorType(int x){
+    if (x == 0){
+        return "Illegal Character";
+    }
+    else if (x == 1){
+        return "Missing Quotation Mark";
+    }
+    else if (x == 2){
+        return "Unexpected End Of File";
+    }
+    else{
+        return "No Error";
+    }
+}
+
+std::ostream& operator<<(std::ostream &os, const Error &error) {
+    return os << std::setw(15) << std::left << "Error Type: " << printErrorType(error.index()) << std::endl;
+}
+
+std::ostream& operator<<(std::ostream &os, const TokenWithError &twe){
+    return os << twe.first << twe.second << std::endl;
+}
 
 bool isDigit(char c){
     return (c >= '0' && c <= '9');
@@ -148,7 +179,9 @@ std::string peekNCharacters(std::ifstream &input, int n){
 }
 
 //Given that the next token to be scanned is a number, scan the number
-Token scanNumberOrHex(std::ifstream &input){
+TokenWithError scanNumberOrHex(std::ifstream &input){
+    //this will be used later when creating a TokenWithError to return
+    Token tok;
     char peek = (char) input.peek();
     assert(isDigit(peek));
     if (peekNCharacters(input, 2) == "0x"){
@@ -159,7 +192,8 @@ Token scanNumberOrHex(std::ifstream &input){
             input.ignore(1);
         }
         if (isHexString(hexString)){
-            return {literal, hexString};
+            tok = {literal, hexString};
+            return std::make_pair(tok, std::monostate{});
         }
         //we should probably print out some error if this happens, because a "0x" character at the start of a 
         // token should never happen
@@ -177,11 +211,14 @@ Token scanNumberOrHex(std::ifstream &input){
         input.ignore(1);
         peek = (char) input.peek();
     }
-    return {number, next_num};
+    tok = {number, next_num};
+    return make_pair(tok, std::monostate{});
 }
 
 //Given that the next token to be scanned is a word, scan the word
-Token scanWord(std::ifstream &input){
+TokenWithError scanWord(std::ifstream &input){
+    //this will be used later when creating a TokenWithError to return
+    Token tok;
     char peek = (char) input.peek();
     assert(isLetter(peek));
     std::string next_word = "";
@@ -190,10 +227,14 @@ Token scanWord(std::ifstream &input){
         input.ignore(1);
         peek = (char) input.peek();
     }
-    return {word, next_word};
+    tok = {word, next_word};
+    return {tok, std::monostate{}};
 }
 
-Token scanCharLiteral(std::ifstream &input){
+TokenWithError scanCharLiteral(std::ifstream &input){
+    //this will be used later when creating a TokenWithError to return
+    Token tok;
+    Error err = std::monostate{};
     assert(input.peek() == '\'');
     input.ignore(1);
     char charLiteralVal = input.peek();
@@ -203,45 +244,67 @@ Token scanCharLiteral(std::ifstream &input){
         // '\t'
         if (nextTwoChars == "t\'"){
             input.ignore(2);
-            return {literal, '\t'};
+            tok = {literal, '\t'};
         }
         // '\n'
         else if (nextTwoChars == "n\'"){
             input.ignore(2);
-            return {literal, '\n'};
+            tok = {literal, '\n'};
         }
         // '\''
         else if (nextTwoChars == "\'\'"){
             input.ignore(2);
-            return {literal, '\''};
+            tok = {literal, '\''};
         }
         // '\"'
         else if (nextTwoChars == "\"\'"){
             input.ignore(2);
-            return {literal, '\"'};
+            tok = {literal, '\"'};
         }
         // '\\'
-        else{
-            assert(nextTwoChars == "\\\'");
+        else if (nextTwoChars == "\\\'"){
             input.ignore(2);
-            return {literal, '\\'};
+            tok = {literal, '\\'};
         }
+        else{
+            IllegalChar err2;
+            err2.unexpectedChar = input.peek();
+            err = err2;
+        }
+        return {tok, err};
     }
     else{
-        assert(isValidNormalChar(charLiteralVal));
+        if (!isValidNormalChar(charLiteralVal)){
+            IllegalChar err2;
+            err2.unexpectedChar = input.peek();
+            err = err2;
+        }
         input.ignore(1);
-        assert(input.peek() == '\'');
+        if (input.peek() != '\''){
+            IllegalChar err2;
+            err2.unexpectedChar = input.peek();
+            err = err2;
+        }
         input.ignore(1);
-        return {literal, charLiteralVal};
+        tok = {literal, charLiteralVal};
+        return {tok, err};
     }
 }
 
-Token scanStringLiteral(std::ifstream &input){
+TokenWithError scanStringLiteral(std::ifstream &input){
+    Token tok;
+    Error err = std::monostate{};
     assert(input.peek() == '\"');
     input.ignore(1);
     char nextChar = input.peek();
     std::string stringVal = "";
-    while (nextChar != '\"' && !input.eof()){
+    while (nextChar != '\"'){
+        if (input.eof()){
+            UnexpectedEOF err2;
+            err = err2;
+            tok = {literal, stringVal};
+            return make_pair(tok, err);
+        }
         if (nextChar == '\\'){
             std::string nextTwoChars = peekNCharacters(input, 2);
             // '\t'
@@ -276,14 +339,22 @@ Token scanStringLiteral(std::ifstream &input){
             nextChar = input.peek();
         }
         else{
-            assert(isValidNormalChar(nextChar));
-            stringVal += nextChar;
-            input.ignore(1);
-            nextChar = input.peek();
+
+            if (isValidNormalChar(nextChar)){
+                stringVal += nextChar;
+                input.ignore(1);
+                nextChar = input.peek();
+            }
+            else{
+                IllegalChar err2;
+                err2.unexpectedChar = input.peek();
+                err = err2;
+            }
         }
     }
     input.ignore(1);
-    return {literal, stringVal};
+    tok = {literal, stringVal};
+    return std::make_pair(tok, err);
 }
 
 //When there's a line comment, move the ifstream to the beginning of the next line
@@ -295,31 +366,41 @@ void skipComment(std::ifstream &input){
     input.ignore(1);
 }
 
-Token scanSymbol(std::ifstream &input){
+TokenWithError scanSymbol(std::ifstream &input){
     std::string twoChars = peekNCharacters(input, 2);
+    Token tok;
+    Error err = std::monostate{};
     for (int idx = 0; idx < 8; idx++){
         if (twoChars == symbolMatcher[idx].first){
             input.ignore(2);
-            return symbolMatcher[idx].second;
+            tok = symbolMatcher[idx].second;
+            return std::make_pair(tok, err);
         }
     }
     for (int idx = 8; idx < symbolMatcher.size(); idx++){
         if (twoChars.substr(0, 1) == symbolMatcher[idx].first){
             input.ignore(1);
-            return symbolMatcher[idx].second;
+            tok = symbolMatcher[idx].second;
+            return std::make_pair(tok, err);
         }
     }
-    return {eof, std::monostate{}};
+    tok = {eof, std::monostate{}};
+    return std::make_pair(tok, err);
 }
 
-Token scanOneToken(std::ifstream &input){
+TokenWithError scanOneToken(std::ifstream &input){
     if (input.peek() == EOF || input.eof()){
-       return {eof, std::monostate{}};
+       Token tok = {eof, std::monostate{}};
+       Error err = std::monostate{};
+       return {tok, err};
     }
+    Token tok;
+    Error err = std::monostate{};
     char peek = (char) input.peek();
     for (; ; peek = (char) input.peek()){
         if (input.peek() == EOF || input.eof()){
-            return {eof, std::monostate{}};
+            tok = {eof, std::monostate{}};
+            return make_pair(tok, err);
         }
         if (peek == ' ' || peek == '\t' || peek == '\n'){
             input.ignore(1);
@@ -343,8 +424,8 @@ Token scanOneToken(std::ifstream &input){
         }
         //need to implement some check to see we're actually finding an operator
         else{
-            Token sym = scanSymbol(input);
-            if (sym.tag != eof){
+            TokenWithError sym = scanSymbol(input);
+            if (sym.first.tag != eof){
                 return sym;
             }
             else{
@@ -356,14 +437,14 @@ Token scanOneToken(std::ifstream &input){
     }
 }
 
-std::vector<Token> scanner(std::string filename){
+std::vector<TokenWithError> scanner(std::string filename){
     std::ifstream input;
     input.open(filename);
-    std::vector<Token> tokenList;
-    Token nextToken = scanOneToken(input);
-    while (nextToken.tag != eof){
-        tokenList.push_back(nextToken);
-        nextToken = scanOneToken(input);
+    std::vector<TokenWithError> tokenList;
+    TokenWithError nextTokenWithError = scanOneToken(input);
+    while (nextTokenWithError.first.tag != eof){
+        tokenList.push_back(nextTokenWithError);
+        nextTokenWithError = scanOneToken(input);
     }
     input.close();
     return tokenList;
@@ -372,8 +453,9 @@ std::vector<Token> scanner(std::string filename){
 
 
 int main(){
-    std::vector<Token> tokenVector = scanner("test.in");
-    for (auto token : tokenVector){
-        std::cout << token << std::endl;
+    std::vector<TokenWithError> tokenWithErrorVector = scanner("test.in");
+    for (auto tokenWithError : tokenWithErrorVector){
+        std::cout << tokenWithError << std::endl;
     }
+    
 }
